587002b9791c03b6269f7b64f66b063fd85539bd
Lars Hutzelmann <lhutzelmann@users.noreply.github.com>
2025-04-10 17:08:18 +0200

    Correct path to SID file

--- scripts/load_sid_file.py
+++ scripts/load_sid_file.py
@@ -3,7 +3,7 @@ import pathlib
 from sid_edit_ui.datafields.sidfile import SIDFile
 
 if __name__ == "__main__":
-    sid_file_path = pathlib.Path("Jump_2SID.sid")
+    sid_file_path = pathlib.Path("Metal_Dust_Title_Remix.sid")
     sid_file_data: bytes = sid_file_path.read_bytes()
     sid_file = SIDFile.from_sid(sid_file_data)
     print(sid_file.model_dump_json(indent=2))
